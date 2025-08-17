42f540e5c6fc4011351eff2bc9c6e612faf25992
danthesupermin275 <danthemin44@gmail.com>
2025-08-17 12:19:01 -0300

    fixed toast manager + other broken things

--- scripts/project exploer/settings_button.gd
+++ scripts/project exploer/settings_button.gd
@@ -10,7 +10,6 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-
-func _on__settings_pressed() -> void:
-	ToastManager.toast("test", 1)
+func _on_pressed() -> void:
+	ToastManager.toast("test, should show settings", 4)
 	$Settings.show()
